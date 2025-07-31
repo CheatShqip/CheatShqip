3e60f4f63697afb19d26adf30138d045b2f992a3
Thomas Boutin <thomas.boutin@externe.systeme-u.fr>
2025-07-31 17:06:19 +0200

    :sparkles: bump version and version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.cheatshqip"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.0.1"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
