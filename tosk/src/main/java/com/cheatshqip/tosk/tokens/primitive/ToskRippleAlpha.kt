a213d224880c7507de5b77c41274988f42a61bc5
Thomas Boutin <thomas.boutin@externe.systeme-u.fr>
2025-08-29 18:28:00 +0200

    :arrow_down: downgrade gradle

--- tosk/src/main/java/com/cheatshqip/tosk/tokens/primitive/ToskRippleAlpha.kt
+++ tosk/src/main/java/com/cheatshqip/tosk/tokens/primitive/ToskRippleAlpha.kt
@@ -3,7 +3,22 @@ package com.cheatshqip.tosk.tokens.primitive
 import androidx.compose.material.ripple.RippleAlpha
 
 object ToskRippleAlpha {
-    val S = RippleAlpha(0.1f, 0.1f, 0.1f, 0.1f)
-    val M = RippleAlpha(0.3f, 0.3f, 0.3f, 0.3f)
-    val L = RippleAlpha(0.5f, 0.5f, 0.5f, 0.5f)
+    val S = RippleAlpha(
+        draggedAlpha = 0.1f,
+        focusedAlpha = 0.1f,
+        hoveredAlpha = 0.1f,
+        pressedAlpha = 0.1f,
+    )
+    val M = RippleAlpha(
+        draggedAlpha = 0.3f,
+        focusedAlpha = 0.3f,
+        hoveredAlpha = 0.3f,
+        pressedAlpha = 0.3f,
+    )
+    val L = RippleAlpha(
+        draggedAlpha = 0.5f,
+        focusedAlpha = 0.5f,
+        hoveredAlpha = 0.5f,
+        pressedAlpha = 0.5f
+    )
 }
