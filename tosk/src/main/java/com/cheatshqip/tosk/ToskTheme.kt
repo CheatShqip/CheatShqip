4e39846e47b18f7a483781ee757c3628f7818f30
Thomas Boutin <thomas.boutin@externe.systeme-u.fr>
2025-09-10 17:07:17 +0200

    :recycle: rearrange code in Theme

--- tosk/src/main/java/com/cheatshqip/tosk/ToskTheme.kt
+++ tosk/src/main/java/com/cheatshqip/tosk/ToskTheme.kt
@@ -11,10 +11,20 @@ import com.cheatshqip.tosk.tokens.semantic.ToskColors
 import com.cheatshqip.tosk.tokens.semantic.ToskTypography
 
 private val LocalToskTypography = staticCompositionLocalOf<ToskTypography> {
-    error("Wrap you content with ToskTheme {} to get access to Backpack typography")
+    error("Wrap you content with ToskTheme {} to get access to Tosk typography")
 }
 private val LocalToskColors = staticCompositionLocalOf<ToskColors> {
-    error("Wrap you content with ToskTheme {} to get access to Backpack colors")
+    error("Wrap you content with ToskTheme {} to get access to Tosk colors")
+}
+
+@ReadOnlyComposable
+@Composable
+private fun lightDarkColors(): ToskColors {
+    return if (isSystemInDarkTheme()) {
+        ToskColors.Dark
+    } else {
+        ToskColors.Light
+    }
 }
 
 @Composable
@@ -22,7 +32,7 @@ fun ToskTheme(
     content: @Composable () -> Unit,
 ) {
     val typography = ToskTypography.Default
-    val colors = if (isSystemInDarkTheme()) ToskColors.Dark else ToskColors.Light
+    val colors = lightDarkColors()
 
     CompositionLocalProvider(
         LocalToskTypography provides typography,
@@ -41,16 +51,14 @@ object ToskTheme {
         } else {
             LocalToskTypography.current
         }
-
     val colors: ToskColors
         @Composable
         @ReadOnlyComposable
         get() = if (LocalInspectionMode.current) {
-            if (isSystemInDarkTheme()) ToskColors.Dark else ToskColors.Light
+            lightDarkColors()
         } else {
             LocalToskColors.current
         }
-
     val spacing: ToskSpacing
         @ReadOnlyComposable
         @Composable
