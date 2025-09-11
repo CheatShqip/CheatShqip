1b8b526fad33b0361b7b448387d2326d413a29b0
Thomas Boutin <thomas.boutin@externe.systeme-u.fr>
2025-09-11 11:29:53 +0200

    :sparkles: add ui test

---
+++ app/src/androidTest/java/com/cheatshqip/HomeScreenTest.kt
@@ -0,0 +1,39 @@
+package com.cheatshqip
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class HomeScreenTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun homeScreen_topAppBar_displaysCorrectTitle() {
+        composeTestRule.onNodeWithText("CheatShqip").assertIsDisplayed()
+    }
+
+    @Test
+    fun homeScreen_translateCard_displaysKarte() {
+        composeTestRule.onNodeWithText("Word").performTextInput("card")
+
+        composeTestRule.onNodeWithContentDescription(label = "Translate").performClick()
+
+        composeTestRule.waitUntil(timeoutMillis = 15_000) {
+            composeTestRule.onAllNodesWithText("kartë")
+                .fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onNodeWithText("kartë").assertIsDisplayed()
+    }
+}
