ebdc6a83c867437b21ad6f3f5c09d6035459c176
Thomas Boutin <tboutin@ippon.fr>
2025-11-07 16:47:56 +0100

    :recycle: refactor button size magic number

--- tosk/src/main/java/com/cheatshqip/tosk/button/tokens/ToskButtonSize.kt
+++ tosk/src/main/java/com/cheatshqip/tosk/button/tokens/ToskButtonSize.kt
@@ -5,6 +5,8 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.cheatshqip.tosk.tokens.primitive.ToskSpacing
 
+private const val MINIMUM_BUTTON_HEIGHT = 40
+
 sealed class ToskButtonSize(
     val minHeight: Dp,
     val contentPadding: PaddingValues = PaddingValues(
@@ -12,6 +14,6 @@ sealed class ToskButtonSize(
         horizontal = ToskSpacing.M
     )
 ) {
-    data object Medium : ToskButtonSize(minHeight = 40.dp)
+    data object Medium : ToskButtonSize(minHeight = MINIMUM_BUTTON_HEIGHT.dp)
 }
 
